411e6a22a1f2797d1a4d269a89a75c03c0dce058
maixuanhieu135 <111688465+maixuanhieu135@users.noreply.github.com>
2025-02-09 22:16:38 +0700

    test

--- datn-fe/src/App.js
+++ datn-fe/src/App.js
@@ -1,13 +1,14 @@
-import './i18n'; // Import your i18n.js file here
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
+import "./i18n"; // Import your i18n.js file here
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
       </Routes>
